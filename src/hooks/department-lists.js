e5d3e43a5d209c7da952e325ca044fb770094cf2
dillionmegida <dillionmegida@gmail.com>
2020-01-18 16:12:52 +0100

    Rephrase query

--- src/hooks/department-lists.js
+++ src/hooks/department-lists.js
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql } from 'gatsby';
 export default () => {
     const data = useStaticQuery(graphql`
         {
-            allDirectory(filter: {relativeDirectory: {eq: "pages"}}) {
+            allDirectory(filter: {relativeDirectory: {eq: ""}}) {
                 edges {
                     node {
                         id
@@ -11,14 +11,14 @@ export default () => {
                     }
                 }
             }
-        }
+        }      
     
     `)
-
+    
     return data.allDirectory.edges.map(({node}) => (
         {
             id: node.id,
             name: node.name
         }
-    ));
-}
+        ));
+    }
