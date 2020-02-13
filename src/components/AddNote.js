cb84ff7a1bcabadcaa20f6251b1a327a71ab0a0d
dillionmegida <dillionmegida@gmail.com>
2020-02-13 01:23:40 +0100

    Remove unused literal

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const addDoc = {
-    link: 'https://docs.google.com/document/d/1pXt9tNdFeb3i4-t86tQenX5gIZ7-qCHyAxrvKa1HvsI/edit?usp=sharing'
-}
+// Not in use for now
+// const addDoc = {
+//     link: 'https://docs.google.com/document/d/1pXt9tNdFeb3i4-t86tQenX5gIZ7-qCHyAxrvKa1HvsI/edit?usp=sharing'
+// }
 
 export default () => (
     <p>
